562686e99c3fe55174a733fe734d50e10f539aee
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2024-07-18 13:52:14 -0600

    soluciona de problemas

--- Operaciones de numeros infinitos static/Multiplicar.cpp
+++ Operaciones de numeros infinitos static/Multiplicar.cpp
@@ -59,12 +59,6 @@ void Multiplicacion::Conversion(vector<int> resultadoint)
 {
     /*-----------------INICIO-CONVERSION--------------------*/
 
-    for (auto i : resultadoint)
-    {
-        cout << i;
-    }
-    cout << endl;
-
     size_t t = resultadoint.size();
     resultado = new char[t + 3];
     int inicio = 0;
